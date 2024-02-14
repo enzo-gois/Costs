07d0fd736bc09f2936d2f9560a9d894d27fc8546
enzo-gois <enzoab26@outlook.com>
2024-02-14 18:34:50 -0300

    projeto final

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Projects'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Project from './components/pages/Project'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />}> </Route>
+          <Route path="/projects" element={<Projects />}> </Route>
+          <Route path="/company" element={<Company />}> </Route>
+          <Route path="/contact" element={<Contact />}> </Route>
+          <Route path="/newproject" element={<NewProject />}> </Route>
+          <Route path="/project/:id" element={<Project />}> </Route>
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App;
